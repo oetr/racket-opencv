fb2cd3a77b729233daa17e20c2db03f43feffd92
Petr Samarin <petrsamarin@gmail.com>
2012-05-14 00:40:05 +0200

    First step to testing.

--- Examples/VideoSimple.rkt
+++ Examples/VideoSimple.rkt
@@ -9,17 +9,20 @@
 ;; Tested with iSight camera of my MacBook Pro
 
 ;;; Includes
-(require "../src/types.rkt")
-(require "../src/core.rkt")
-(require "../src/highgui.rkt")
-(require "../src/imgproc.rkt")
-
+(require "../src/types.rkt"
+         "../src/core.rkt"
+         "../src/highgui.rkt"
+         "../src/imgproc.rkt")
 
 (define capture (cvCaptureFromCAM 0))
 
 ;; Reduce image resolution to 640x480
-(define success? (cvSetCaptureProperty capture CV_CAP_PROP_FRAME_WIDTH 640.0))
-(set! success? (cvSetCaptureProperty capture CV_CAP_PROP_FRAME_HEIGHT 480.0))
+;; for some reason, if the value returned by a C function is not void,
+;; it is implicitly printed out, without being commanded to!
+;; to prevent a print out by default, we bind returned value to a symbol
+(define param-set #f)
+(set! param-set (cvSetCaptureProperty capture CV_CAP_PROP_FRAME_WIDTH 640.0))
+(set! param-set (cvSetCaptureProperty capture CV_CAP_PROP_FRAME_HEIGHT 480.0))
 
 ;; Capture an image to get parameters
 (define captured-image (cvQueryFrame capture))
@@ -34,27 +37,14 @@
 
 ;; Init an IplImage to where captured images will be copied
 (define frame (cvCreateImage size depth channels))
-(printf "depth = ~a~n" depth)
-(define grey-frame (cvCreateImage size IPL_DEPTH_8U 1))
-
-(define corners (cvCreateImage size IPL_DEPTH_32F 1))
 
 (let loop ()  
   (set! captured-image (cvQueryFrame capture))
-  (cvCvtColor captured-image grey-frame CV_BGR2GRAY)
-  ;;(cvCopy captured-image frame #f)
-  ;; (cvErode grey-frame grey-frame #f 1)
-  ;; (cvDilate grey-frame grey-frame #f 1)
-  ;; (cvErode grey-frame grey-frame #f 1)
-  ;; (cvDilate grey-frame grey-frame #f 1)
-  ;; (cvCanny grey-frame grey-frame 30.0 30.0 3)
-  ;; (cvCanny grey-frame grey-frame 100.0 100.0 3)
-  ;;(cvErode grey-frame grey-frame #f 10)
-  ;;(cvSmooth grey-frame grey-frame CV_GAUSSIAN 5 5 0.0 0.0)
-  (cvCornerHarris grey-frame corners 5 7 0.001)
-  (cvShowImage "Video Capture" corners)
+  (cvCopy captured-image frame #f)
+  (cvShowImage "Video Capture" frame)
   (unless (>= (cvWaitKey 1) 0)
     (loop)))
 
+;; clean up
 (cvReleaseCapture capture)
 (cvDestroyWindow "Video Capture")
